60faa7877040373a7ad161527c3ef6e415a7c457
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2025-02-05 15:58:42 +0800

    Extract LINE auth flow to a middleware

--- internal/rest/routes.go
+++ internal/rest/routes.go
@@ -12,33 +12,36 @@ import (
 type channelSecret = string
 
 func AddRoutes(r *gin.Engine, s channelSecret) {
+	r.Use(lineAuthMiddleware(s))
 	r.POST("/api/v1/callback", func(ctx *gin.Context) {
+		ctx.Status(200)
+	})
+}
+
+func lineAuthMiddleware(s channelSecret) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
 		if ctx.Request.Body == nil {
-			ctx.Status(400)
-			return
+			ctx.AbortWithStatus(400)
 		}
 
 		defer ctx.Request.Body.Close()
 
 		body, err := io.ReadAll(ctx.Request.Body)
 		if err != nil || len(body) == 0 {
-			ctx.Status(400)
-			return
+			ctx.AbortWithStatus(400)
 		}
 
 		decoded, err := base64.StdEncoding.DecodeString(ctx.Request.Header.Get("x-line-signature"))
 		if err != nil {
-			ctx.Status(401)
-			return
+			ctx.AbortWithStatus(401)
 		}
 
 		hash := hmac.New(sha256.New, []byte(s))
 		hash.Write(body)
 		if !hmac.Equal(decoded, hash.Sum(nil)) {
-			ctx.Status(401)
-			return
+			ctx.AbortWithStatus(401)
 		}
 
-		ctx.Status(200)
-	})
+		ctx.Next()
+	}
 }
