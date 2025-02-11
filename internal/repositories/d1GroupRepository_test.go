6b31612ae052699003d6bf095a9c4541418bc108
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2025-02-11 14:07:25 +0800

    Test request headers sent by D1GroupRepository

--- internal/repositories/d1GroupRepository_test.go
+++ internal/repositories/d1GroupRepository_test.go
@@ -43,31 +43,62 @@ func TestSaveGroupParams_MarshalJSON(t *testing.T) {
 
 func TestD1GroupRepository_Save(t *testing.T) {
 	testCases := []struct {
-		name          string
-		statusCode    int
-		expectError   bool
-		expectedError error
+		name               string
+		expectedReqHeaders map[string]string
+		expectedRespStatus int
+		expectError        bool
+		expectedError      error
 	}{
-		{"Returns 204 (Success)", http.StatusNoContent, false, nil},
-		{"Returns 304 (Success)", http.StatusNotModified, true, r.ErrorAlreadyExists},
-		{"Returns 400 (Failure)", http.StatusBadRequest, true, fmt.Errorf("unexpected response status: %d", http.StatusBadRequest)},
-		{"Returns 500 (Failure)", http.StatusInternalServerError, true, fmt.Errorf("unexpected response status: %d", http.StatusInternalServerError)},
+		{
+			name:               "Returns 204 (Success)",
+			expectedReqHeaders: map[string]string{"Content-Type": "application/json"},
+			expectedRespStatus: http.StatusNoContent,
+			expectError:        false,
+		},
+		{
+			name:               "Returns 304 (Success)",
+			expectedReqHeaders: map[string]string{"Content-Type": "application/json"},
+			expectedRespStatus: http.StatusNotModified,
+			expectError:        true,
+			expectedError:      r.ErrorAlreadyExists,
+		},
+		{
+			name:               "Returns 400 (Failure)",
+			expectedReqHeaders: map[string]string{"Content-Type": "application/json"},
+			expectedRespStatus: http.StatusBadRequest,
+			expectError:        true,
+			expectedError:      fmt.Errorf("unexpected response status: %d", http.StatusBadRequest),
+		},
+		{
+			name:               "Returns 500 (Failure)",
+			expectedReqHeaders: map[string]string{"Content-Type": "application/json"},
+			expectedRespStatus: http.StatusInternalServerError,
+			expectError:        true,
+			expectedError:      fmt.Errorf("unexpected response status: %d", http.StatusInternalServerError),
+		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 
-			mockClient := u.NewMockHttpClient(tc.statusCode)
+			var hs http.Header
+			mockClient := u.NewMockHttpClient(tc.expectedRespStatus, func(req *http.Request) {
+				hs = req.Header
+			})
 			repo := r.NewD1GroupRepository("https://example.com/api/v1/groups", mockClient)
 
 			group := &g.Group{Id: "C1234f49365c6b492b337189e3343a9d9"}
 			result, err := repo.Save(group)
 
+			for k, v := range tc.expectedReqHeaders {
+				assert.Equal(t, v, hs.Get(k), "Expected header %s to be %s, but got %s", k, v, hs.Get(k))
+			}
+
 			if tc.expectError {
-				assert.Error(t, err, "Expected an error but got none for status code %d", tc.statusCode)
+				assert.Error(t, err, "Expected an error but got none for status code %d", tc.expectedRespStatus)
 				assert.Nil(t, result, "Expected result to be nil on error, but got: %v", result)
-				if tc.statusCode == http.StatusNotModified {
+				if tc.expectedRespStatus == http.StatusNotModified {
 					assert.Equal(t, tc.expectedError, err, "Expected error to be ErrorAlreadyExists but got: %v", err)
 				}
 			} else {
@@ -81,28 +112,32 @@ func TestD1GroupRepository_Save(t *testing.T) {
 
 func TestD1GroupRepository_Destroy(t *testing.T) {
 	tcs := []struct {
-		name          string
-		statusCode    int
-		shouldFail    bool
-		expectedError error
+		name               string
+		expectedReqHeaders map[string]string
+		expectedRespStatus int
+		shouldFail         bool
+		expectedError      error
 	}{
 		{
-			name:          "Returns nil on successful delete",
-			statusCode:    http.StatusNoContent,
-			shouldFail:    false,
-			expectedError: nil,
+			name:               "Returns nil on successful delete",
+			expectedReqHeaders: map[string]string{"Content-Type": "application/json"},
+			expectedRespStatus: http.StatusNoContent,
+			shouldFail:         false,
+			expectedError:      nil,
 		},
 		{
-			name:          "Returns ErrorNotFound when group does not exist",
-			statusCode:    http.StatusNotFound,
-			shouldFail:    false,
-			expectedError: r.ErrorNotFound,
+			name:               "Returns ErrorNotFound when group does not exist",
+			expectedReqHeaders: map[string]string{"Content-Type": "application/json"},
+			expectedRespStatus: http.StatusNotFound,
+			shouldFail:         false,
+			expectedError:      r.ErrorNotFound,
 		},
 		{
-			name:          "Returns an error on unexpected status code",
-			statusCode:    http.StatusInternalServerError,
-			shouldFail:    false,
-			expectedError: fmt.Errorf("unexpected response status: %d", http.StatusInternalServerError),
+			name:               "Returns an error on unexpected status code",
+			expectedReqHeaders: map[string]string{"Content-Type": "application/json"},
+			expectedRespStatus: http.StatusInternalServerError,
+			shouldFail:         false,
+			expectedError:      fmt.Errorf("unexpected response status: %d", http.StatusInternalServerError),
 		},
 	}
 
@@ -110,17 +145,24 @@ func TestD1GroupRepository_Destroy(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 
-			mockClient := u.NewMockHttpClient(tc.statusCode)
+			var hs http.Header
+			mockClient := u.NewMockHttpClient(tc.expectedRespStatus, func(req *http.Request) {
+				hs = req.Header
+			})
 			repo := r.NewD1GroupRepository("https://example.com/api/v1/groups", mockClient)
 
 			group := &g.Group{Id: "C1234f49365c6b492b337189e3343a9d9"}
 
 			err := repo.Destroy(group)
 
+			for k, v := range tc.expectedReqHeaders {
+				assert.Equal(t, v, hs.Get(k), "Expected header %s to be %s, but got %s", k, v, hs.Get(k))
+			}
+
 			if tc.expectedError == nil {
 				assert.NoError(t, err, "Expected no error but got one: %v", err)
 			} else {
-				assert.Error(t, err, "Expected an error but got none for status code %d", tc.statusCode)
+				assert.Error(t, err, "Expected an error but got none for status code %d", tc.expectedRespStatus)
 			}
 		})
 	}
