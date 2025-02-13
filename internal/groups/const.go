7d1345b884a6ef7a0e4c77e2666837dba907343d
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2025-02-13 18:55:06 +0800

    Update MsgAlreadyRegistered text

--- internal/groups/const.go
+++ internal/groups/const.go
@@ -2,4 +2,4 @@ package groups
 
 const MsgRegisterMyGroup = "我需要好好靈修"
 const MsgRegistrationOk = "已加入推播清單。"
-const MsgAlreadyRegistered = "已在加入推播清單中。 :D"
+const MsgAlreadyRegistered = "你已在推播清單中。 :D"
